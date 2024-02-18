ee0ec7dffc12e40e019add3ef85731720faa286f
Nosiss <diogo.roque@justciencias.pt>
2024-02-18 18:06:26 +0000

    create account warns and some documentation

--- app/src/main/java/com/example/globalpc/RegisterActivity.kt
+++ app/src/main/java/com/example/globalpc/RegisterActivity.kt
@@ -3,6 +3,7 @@ package com.example.globalpc
 import android.app.Activity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -19,6 +20,57 @@ class RegisterActivity : Activity() {
         auth = Firebase.auth
     }
 
+    /* GET/SET METHOD DECLARIATION*/
+
+    /**
+     * Returns the name entered
+     * @return - name as a String
+     * */
+    private fun getName(): String {
+        val usernameInput = findViewById<EditText>(R.id.usernameInput)
+
+        return usernameInput.text.toString()
+    }
+
+    /**
+     * Returns the email entered
+     * @return - email as a String
+     * */
+    private fun getEmail(): String {
+        val emailInput = findViewById<EditText>(R.id.emailInput)
+
+        return emailInput.text.toString()
+    }
+
+    /**
+     * Returns the password entered
+     * @return - password as a String
+     * */
+    private fun getPassword(): String {
+        val passwordInput = findViewById<EditText>(R.id.passwordInput)
+
+        return passwordInput.text.toString()
+    }
+
+    /**
+     * Returns the password entered
+     * @return - password as a String
+     * */
+    private fun getConfirmPassword(): String {
+        val passwordInput = findViewById<EditText>(R.id.repeatPasswordInput)
+
+        return passwordInput.text.toString()
+    }
+
+
+
+    /* METHODS DECLARIATION*/
+
+    /**
+     * Create a account in firebase
+     * @param email - Email as String
+     * @param password - Password as String
+     * */
     private fun createAccount(email: String, password: String) {
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
@@ -31,6 +83,10 @@ class RegisterActivity : Activity() {
             }
     }
 
+    /**
+     * Send a Toast notification to mobile screen
+     * @param mensagem - notification content as String
+     * */
     private fun exibirToast(mensagem: String) {
         // Cria e exibe o Toast
         Toast.makeText(this, mensagem, Toast.LENGTH_SHORT).show()
